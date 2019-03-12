f0ec5213d7a771dce4eaadaade4d39cb97dd48e3
ThaDocIzN <rodan72@msn.com>
2019-03-12 04:41:05 -0500

    created instance snacks

--- src/Main.kt
+++ src/Main.kt
@@ -6,4 +6,11 @@ fun main(){
     val food = VendingMachine(1, "Food")
     val drink = VendingMachine( 2, "Drink")
     val office = VendingMachine(3, "Office")
+
+    val chips = Snack(1, "Chips", 36, 1.75, 1)
+    val chocolateBar = Snack(2, "Chocolate Bar", 36, 1.00, 1)
+    val pretzels = Snack(3, "Pretzel", 30, 2.00, 1)
+
+    val soda = Snack(4, "Soda", 24, 2.50, 2)
+    val water = Snack(5, "Water", 20,2.75, 2)
 }
