bf73a11c6347214f6e74d0119d59b9079b297765
ThaDocIzN <rodan72@msn.com>
2019-03-12 04:36:39 -0500

    created instance vending machine

--- src/Main.kt
+++ src/Main.kt
@@ -2,4 +2,8 @@ fun main(){
 
     val jane = Customer(1, "Jane", 45.25)
     val bob = Customer(2,"Bob", 33.14)
+
+    val food = VendingMachine(1, "Food")
+    val drink = VendingMachine( 2, "Drink")
+    val office = VendingMachine(3, "Office")
 }
